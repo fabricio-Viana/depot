397d5c6dc90b2aec519246ceb727cbf17f84f43d
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-03-25 17:20:32 -0300

    improve button and translate texts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -25,7 +25,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: "Cart was successfully created." }
+        format.html { redirect_to @cart, notice: "Carrinho foi criado com sucesso." }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class CartsController < ApplicationController
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: "Cart was successfully updated." }
+        format.html { redirect_to @cart, notice: "Carrinho foi atualizado com sucesso." }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class CartsController < ApplicationController
     @cart.destroy
 
     respond_to do |format|
-      format.html { redirect_to carts_path, status: :see_other, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to carts_path, status: :see_other, notice: "Carrinho foi deletado com sucesso." }
       format.json { head :no_content }
     end
   end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to cart_url(@line_item.cart),
-          notice: "Line item was successfully created." }
+          notice: "Lista de itens foi criada." }
         format.json { render :show,
           status: :created, location: @line_item }
       else
