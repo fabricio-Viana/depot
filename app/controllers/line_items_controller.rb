c1168caf00cdca582a7039bf62cb51559d9689da
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-03-26 16:26:16 -0300

    fix error in log

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,7 +25,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
-    
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to cart_url(@line_item.cart),
@@ -72,6 +72,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
