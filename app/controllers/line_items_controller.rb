1125854f322ade67b3d838dfc01a3f8f1f18cc22
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-04-02 00:22:13 -0300

    add cart to sidebar

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,7 +59,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to cart_url(@line_item.cart) }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
