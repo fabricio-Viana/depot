bae65485e663f71588a4abbc2e5042d01fa69cf3
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-04-09 02:40:30 -0300

    implement validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ApplicationRecord
     "Cartão de Credito" => 1, 
     "PIX" => 2
   }
+  has_many :line_items, dependent: :destroy
+  # ...
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
