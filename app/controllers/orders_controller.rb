429ef93fb377f9245243929409cf6691426bf95a
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-04-09 00:44:25 -0300

    add checkout button and fix :pay_type signal error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,6 +65,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:name, :address, :email, : pay_type)
+      params.require(:order).permit(:name, :address, :email, :pay_type)
     end
 end
